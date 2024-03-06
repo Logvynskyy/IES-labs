2229ca10175e511c2d9437bb46bb806e72adc60a
Logvynskyy <shimon511bm@gmail.com>
2024-03-06 18:02:17 +0100

    fully working lab4, returned necessary topics to mqtt, implemented condition when processing agent data

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -12,4 +12,16 @@ def process_agent_data(
     Returns:
         processed_data_batch (ProcessedAgentData): Processed data containing the classified state of the road surface and agent data.
     """
-    # Implement it
+    # Perform analysis based on accelerometer data to classify road condition
+    if agent_data.accelerometer.z > 16000:  # Example condition, adjust as needed
+        road_state = "pothole"
+    else:
+        road_state = "normal"
+
+    # Create a ProcessedAgentData object with classified road condition
+    processed_data = ProcessedAgentData(
+        road_state = road_state,
+        agent_data = agent_data,
+    )
+
+    return processed_data
